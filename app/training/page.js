f3dd2633d4f962a5c8df02f78204af0223eee348
Mohsen-Nazemi <programmer.mohsen@gmail.com>
2024-06-20 12:46:52 +0330

    Protecting Routes Against Unauthenticated Access

--- app/training/page.js
+++ app/training/page.js
@@ -1,6 +1,12 @@
-import { getTrainings } from '@/lib/training';
+import { getTrainings } from "@/lib/training";
 
 export default async function TrainingPage() {
+  const result = await verifyAuth();
+
+  if (!result.user) {
+    return redirect("/");
+  }
+
   const trainingSessions = getTrainings();
 
   return (
